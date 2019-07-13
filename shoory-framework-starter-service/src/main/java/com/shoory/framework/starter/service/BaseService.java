5a70452e6b6a4644b4df087ff1c793cd42aea604
raymond <raymond@raymond-ThinkPad-Edge-E540>
2019-07-13 22:32:48 +0800

    The classes derived by BaseService need requestClass() and responseClass() no longer.

--- shoory-framework-starter-service/src/main/java/com/shoory/framework/starter/service/BaseService.java
+++ shoory-framework-starter-service/src/main/java/com/shoory/framework/starter/service/BaseService.java
@@ -1,12 +1,26 @@
 package com.shoory.framework.starter.service;
 
+import java.lang.reflect.ParameterizedType;
+
 import com.shoory.framework.starter.api.request.BaseRequest;
 import com.shoory.framework.starter.api.response.BaseResponse;
 
 public abstract class BaseService<IN extends BaseRequest, OUT extends BaseResponse> {	
+	private Class<IN> requestClass;
+	private Class<OUT> responseClass;
 
-	public abstract Class<IN> requestClass();
-	public abstract Class<OUT> responseClass();
+	public Class<IN> requestClass() {
+		return requestClass;
+	}
+	public Class<OUT> responseClass() {
+		return responseClass;
+	}
+
+	BaseService() {
+		ParameterizedType ptype = (ParameterizedType) this.getClass().getGenericSuperclass();
+		requestClass = (Class<IN>)ptype.getActualTypeArguments()[0];
+		responseClass = (Class<OUT>)ptype.getActualTypeArguments()[1];
+	}
 	
 	public abstract OUT invoke(IN request);
 }
