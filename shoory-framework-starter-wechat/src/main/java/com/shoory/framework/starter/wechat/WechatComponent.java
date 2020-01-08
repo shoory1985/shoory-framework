db1b9cc10e3e449ae2fe9e0439be1451fbd1b1ec
jaclker <jinghuan326@126.com>
2020-01-08 15:36:19 +0800

    1、更新微信和小程序组件

--- shoory-framework-starter-wechat/src/main/java/com/shoory/framework/starter/wechat/WechatComponent.java
+++ shoory-framework-starter-wechat/src/main/java/com/shoory/framework/starter/wechat/WechatComponent.java
@@ -1,11 +1,8 @@
 package com.shoory.framework.starter.wechat;
 
-
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.stereotype.Component;
-
 
 import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.mp.api.WxMpService;
@@ -19,43 +16,10 @@ public class WechatComponent {
 	@Autowired
 	private WxMpService service;
 
-
-	public String getOpenId(String code) {
-		String openId=null;
-		try {
-			WxMpOAuth2AccessToken accessToken = service.oauth2getAccessToken(code);
-			WxMpUser user = service.oauth2getUserInfo(accessToken, null);
-
-			openId=user.getOpenId();
-		} catch (WxErrorException e) {
-			e.printStackTrace();
-		}
-		return openId;
-	}
-
-	public WechatUserInfo getUserInfo(String code) {
-		WechatUserInfo userInfo=null;
-		try {
-			WxMpOAuth2AccessToken accessToken = service.oauth2getAccessToken(code);
-			WxMpUser user = service.oauth2getUserInfo(accessToken, null);
-			userInfo=new WechatUserInfo();
-			userInfo.setCity(user.getCity());
-			userInfo.setCountry(user.getCountry());
-			userInfo.setHeadImageUrl(user.getHeadImgUrl());
-			userInfo.setNickname(user.getNickname());
-			userInfo.setOpenid(user.getOpenId());
-			userInfo.setProvince(user.getProvince());
-			userInfo.setSex(user.getSex()==1?"男":"女");
-			if(StringUtils.isNotBlank(user.getUnionId())) {
-				userInfo.setUnionId(user.getUnionId());
-			}else {
-				userInfo.setUnionId("");
-			}
-		} catch (WxErrorException e) {
-			e.printStackTrace();
-		}
-
-		return userInfo;
+	public WxMpUser getUserInfo(String code) throws WxErrorException {
+		WxMpOAuth2AccessToken accessToken = service.oauth2getAccessToken(code);
+		WxMpUser user = service.oauth2getUserInfo(accessToken, null);
+		return user;
 	}
 
 	public WechatOpenIdAndUnionId getOpenIdAndUnionId(String code) {
--- shoory-framework-starter-wechatmp/src/main/java/com/shoory/framework/starter/wechatmp/WechatMpComponent.java
+++ shoory-framework-starter-wechatmp/src/main/java/com/shoory/framework/starter/wechatmp/WechatMpComponent.java
@@ -1,18 +1,14 @@
 package com.shoory.framework.starter.wechatmp;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import cn.binarywang.wx.miniapp.api.WxMaService;
-import cn.binarywang.wx.miniapp.api.impl.WxMaServiceImpl;
 import cn.binarywang.wx.miniapp.bean.WxMaJscode2SessionResult;
 import cn.binarywang.wx.miniapp.bean.WxMaPhoneNumberInfo;
 import cn.binarywang.wx.miniapp.bean.WxMaUserInfo;
-import cn.binarywang.wx.miniapp.config.impl.WxMaDefaultConfigImpl;
-import cn.binarywang.wx.miniapp.message.WxMaMessageRouter;
+import me.chanjar.weixin.common.error.WxErrorException;
 
 @Component
 @ConditionalOnBean(value= WechatMpConfig.class)
@@ -20,46 +16,16 @@ public class WechatMpComponent {
 	@Autowired
 	private WxMaService service; 
 	
-	public String getOpenId(String code) {
-		String openId = null;
-		try {
-			WxMaJscode2SessionResult result = service.jsCode2SessionInfo(code);
-			if (result != null) {
-				openId = result.getOpenid();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return openId;
+	public WxMaJscode2SessionResult code2Session(String code) throws WxErrorException {
+		WxMaJscode2SessionResult result = null;
+		return	result = service.jsCode2SessionInfo(code);
 	}
 
-	public WechatMpOpenIdAndUnionId getOpenIdAndUnionId(String code) {
-		WechatMpOpenIdAndUnionId wechatMpOpenIdAndUnionId = null;
-		try {
-			WxMaJscode2SessionResult result = service.jsCode2SessionInfo(code);
-			wechatMpOpenIdAndUnionId = new WechatMpOpenIdAndUnionId();
-			wechatMpOpenIdAndUnionId.setOpenId(result.getOpenid());
-			wechatMpOpenIdAndUnionId.setUnionId(result.getUnionid());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return wechatMpOpenIdAndUnionId;
-	}
 
-	public WechatMpUserInfo getUserInfo(String sessionKey, String encryptedData, String ivStr) {
+
+	public WxMaUserInfo getUserInfo(String sessionKey, String encryptedData, String ivStr) {
 		WxMaUserInfo userInfo = service.getUserService().getUserInfo(sessionKey, encryptedData, ivStr);
-		WechatMpUserInfo wechatMpUserInfo = new WechatMpUserInfo();
-		wechatMpUserInfo.setAvatarUrl(userInfo.getAvatarUrl());
-		wechatMpUserInfo.setCity(userInfo.getCity());
-		wechatMpUserInfo.setCountry(userInfo.getCountry());
-		wechatMpUserInfo.setGender(userInfo.getGender());
-		wechatMpUserInfo.setLanguage(userInfo.getLanguage());
-		wechatMpUserInfo.setNickName(userInfo.getNickName());
-		wechatMpUserInfo.setOpenId(userInfo.getOpenId());
-		wechatMpUserInfo.setProvince(userInfo.getProvince());
-		wechatMpUserInfo.setUnionId(userInfo.getUnionId());
-		wechatMpUserInfo.setWatermark(userInfo.getWatermark());
-		return wechatMpUserInfo;
+		return userInfo;
 	}
 	
 
