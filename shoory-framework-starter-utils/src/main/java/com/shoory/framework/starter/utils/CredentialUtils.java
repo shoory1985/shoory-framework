36aa6b8c981a4be4e0448581161b4674f9120cf3
rui.xu <shoory@qq.com>
2020-01-30 13:36:11 +0800

    升级credentialUtils支持scene场景

--- shoory-framework-starter-utils/src/main/java/com/shoory/framework/starter/utils/CredentialUtils.java
+++ shoory-framework-starter-utils/src/main/java/com/shoory/framework/starter/utils/CredentialUtils.java
@@ -1,19 +1,30 @@
 package com.shoory.framework.starter.utils;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class CredentialUtils {
 	
 	public long getUserId(String credential) {
-		
+		int index = credential.indexOf(":");
+
 		try {
-			return Long.parseLong(credential);
-			
+			return Long.parseLong(index >= 0 ? credential.substring(index + 1) : credential);
 		} catch (NumberFormatException e) {
 			return 0;
 		}
-		
 	}
-
+	public String getScene(String credential) {
+		int index = credential.indexOf(":");
+		return index >= 0 
+				? credential.substring(0, index)
+				: "";
+	}
+	public String makeCredential(String scene, long userId) {
+		return StringUtils.isNotBlank(scene) 
+				? scene + ":" + userId
+				: String.valueOf(userId);
+	}
 }
