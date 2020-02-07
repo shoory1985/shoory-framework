e805c7aa5a5f6f98592e3f04b742c799b57afb0a
rui.xu <shoory@qq.com>
2020-02-07 11:40:08 +0800

    Sms重构

--- shoory-framework-starter-sms/src/main/java/com/shoory/framework/starter/sms/SmsComponent.java
+++ shoory-framework-starter-sms/src/main/java/com/shoory/framework/starter/sms/SmsComponent.java
@@ -1,5 +1,6 @@
 package com.shoory.framework.starter.sms;
 
 public interface SmsComponent {
-
+	public void sendSms(String nationCode, String phoneNumber, String tplId, String[] params) throws Exception;
+	public void sendVoice(String nationCode, String phoneNumber, String tplId, int playtimes) throws Exception;
 }
