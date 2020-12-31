37e041826464e0dd315b606ed164a9b4f22ff7b6
rui.xu <shoory@qq.com>
2020-12-31 23:37:44 +0800

    不要模块兼容

--- shoory-framework-starter-service-document/src/main/java/com/shoory/framework/starter/service/document/utils/MethodUtils.java
+++ shoory-framework-starter-service-document/src/main/java/com/shoory/framework/starter/service/document/utils/MethodUtils.java
@@ -46,10 +46,10 @@ public class MethodUtils {
 					ret.setReturns(fieldUtils.getList(classRequest));
 					//如果module为空，取request所在的包倒数第二段（第一段是request）
 					if (StringUtils.isBlank(ret.getModule())) {
-						String[] pieces = classRequest.getPackageName().split("\\.");
-						if (pieces.length - 2 >= 0) {
-							ret.setModule(pieces[pieces.length - 2]);
-						}
+//						String[] pieces = classRequest.getPackageName().split("\\.");
+//						if (pieces.length - 2 >= 0) {
+//							ret.setModule(pieces[pieces.length - 2]);
+//						}
 					}
 				});
 
